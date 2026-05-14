7241dafbfad15465d1f7e178261330932ba28ee1
Sandeep-177 <sandeepkumarpradhan177@gmail.com>
2026-05-14 23:39:13 +0530

    form data handling code added

--- Note/src/App.jsx
+++ Note/src/App.jsx
@@ -1,17 +1,36 @@
+
+import { useState } from 'react'
 import './App.css'
+import Card from './Card';
 
 function App() {
- 
+  const [firstInput,setFirstinput]=useState('');
+  const [secondInput,setSecondinput]=useState('');
+  // const [datas,setDatas]=useState(null);
+  function go(e) {
+    e.preventDefault();
+  // setDatas({dat1:{firstInput}, dat2:{secondInput}})
+
+  setFirstinput('');
+  setSecondinput('');
+ }
 
   return (
     <>
       <section id="center" className='flex flex-col border p-5 rounded-2xl inset-shadow-amber-900 gap-4 m-auto'>
         <h1>Creating Note</h1>
-        <input className='border text-white p-2 w-3xs' type="text" placeholder='Enter title'/>
-        <input className='border text-white py-10 px-2 w-3xs' type="text" placeholder='Description'/>
-        <button className='bg-blue-600 p-3'>Create</button>
+        <form onSubmit={go} className='flex flex-col gap-4'>
+        <input className='border text-white p-2 w-3xs' type="text" value={firstInput} onChange={(e)=>{
+          setFirstinput(e.target.value);
+        }} placeholder='Enter title'/>
+        <input className='border text-white py-10 px-2 w-3xs' type="text" value={secondInput} onChange={(e)=>{
+          setSecondinput(e.target.value);
+        }} placeholder='Description'/>
+        <button className='bg-blue-600 p-3' >Create</button>
+        </form>
         
       </section>
+      
     </>
   )
 }
---
+++ Note/src/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card(props){
+    return(
+        <>
+        <div>
+            <h1>{props.firs}</h1>
+            <p>{props.secon}</p>
+        </div>
+        </>
+    )
+}
