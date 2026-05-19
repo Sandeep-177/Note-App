6752f6945456a24b6fe147a2eaa3a685fd5c9e61
Sandeep-177 <sandeepkumarpradhan177@gmail.com>
2026-05-19 22:47:46 +0530

    Note card jsx file code updated

--- Note/src/Card.jsx
+++ Note/src/Card.jsx
@@ -4,7 +4,7 @@ export default function Card({data}){
        {
         data.map((n)=>{
             return(
-             <div className="w-60 h-60 border">
+             <div className="w-60 h-60 border rounded-xl">
             <h1 className="wrap-break-word">{n.title}</h1>
             <p className="wrap-break-word">{n.description}</p>
         </div>
